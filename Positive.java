9c7d246947ebe139aa20e088260eb9f977442a41
swe7 <sandhiya7799@gmail.com>
2018-09-03 14:00:04 +0530

    Create Positive.java

---
+++ Positive.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class Positive {
+
+
+ 
+
+    public void check(int number) {
+
+        if (number > 0) {
+
+            System.out.println(number + " is a positive number");
+
+        } else if (number < 0) {
+
+            System.out.println(number + " is a negative number");
+
+        } else {
+
+            System.out.println(number + " is neither positive nor negative");
+
+        }
+
+    }
+
+}
